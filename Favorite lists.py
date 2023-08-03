50352c2d57d8ad5fee0754a59cc8d92c2f983e57
bthapa0711 <136759391+bthapa0711@users.noreply.github.com>
2023-08-03 18:33:35 -0500

    Update Favorite lists.py

--- Favorite lists.py
+++ Favorite lists.py
@@ -1,3 +1,20 @@
+# the first one is creating new model to represent the user's favorite movies. and adding that to models.py
+# models.py
+from django.db import models
+from django.contrib.auth.models import User
+
+class FavoriteMovie(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie_id = models.IntegerField()  # Storing the TMDB movie ID for each favorite movie
+    movie_title = models.CharField(max_length=200)
+    # Add any other fields you want to store for favorite movies
+
+    def __str__(self):
+        return f"{self.user.username} - {self.movie_title}"
+
+---------------------------------------------------------------------
+# this second one is in views.py which we can update the get-movie recommendation by
+# adding handle to favorite list. You can add a new function to handle adding movies, and then update the get_movie_recommendations view to check if a movie is already in the favorite list for the authenticated user
 # views.py
 from django.contrib.auth.decorators import login_required
 from .models import FavoriteMovie
@@ -32,3 +49,16 @@ def get_movie_recommendations(request, actor, director, genre):
         movie['is_favorite'] = FavoriteMovie.objects.filter(user=user, movie_id=movie_id).exists()
 
     return render(request, "CineMatch/recommendations.html", {"movies": top_5_movies})
+
+------------------------------------------------------------
+# Now updating recommendation.html button which will display button to add movies to favorite list:
+-------------------------------------------------
+# Now following code will update the url.py which include new view for adding movies to the favorite list:
+# urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # ... (other patterns)
+    path('add_to_favorite/<int:movie_id>/<str:movie_title>/', views.add_to_favorite, name='add_to_favorite'),
+]
